c29e6a6258149e8cf05558023c508a4a907553d7
Keerthi <ck.keerthi@gmail.com>
2018-07-01 09:56:26 -0400

    Update register.sh

--- bin/register.sh
+++ bin/register.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..30}
+for i in {1..5}
 do
 gcloud iot devices create device$i --project=$PROJECT --region=$REGION --registry=$REGISTRY --public-key path=rsa_cert.pem,type=rs256
 done
