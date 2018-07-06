dd862c459b3e3c316f325c8d59f04dd567673aae
Keerthi <ck.keerthi@gmail.com>
2018-07-06 10:31:50 -0400

    Update create_temp_alert_store.py

--- bin/create_temp_alert_store.py
+++ bin/create_temp_alert_store.py
@@ -2,7 +2,7 @@ import os
 from google.cloud import datastore
 
 client = datastore.Client(os.environ['GCLOUD_PROJECT'])
-for i in range(30):
+for i in range(5):
     incomplete_key = client.key('device')
     device = datastore.Entity(key=incomplete_key)
     device.update({
